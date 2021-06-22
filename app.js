162952ecab7fef5a4c343162d960fcf6a131126f
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2021-06-22 23:53:52 +0600

    writeFile only

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const server = http.createServer( (req,res) => {
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('Message.txt', message);
+            fs.writeFile('Message.txt', message);
         });       
         res.statusCode = 302;
         res.setHeader('Location', '/');
