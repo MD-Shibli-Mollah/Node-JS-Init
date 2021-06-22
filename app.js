99074c42046f43c86f0e657c894c5cc4dbfffbaa
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2021-06-23 00:00:01 +0600

    Err Func Added

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ const server = http.createServer( (req,res) => {
         req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFile('Message.txt', message);
-        });       
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+            fs.writeFile('Message.txt', message, (err) => {
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+            });
+        });
     }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
