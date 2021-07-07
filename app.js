98734e9a09410a09026f00bc42d93a5e70b89ba4
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2021-07-08 02:04:38 +0600

    express POST

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
  
-const routes = require('./routes');
+const app = express();
 
-const server = http.createServer(routes);
-server.listen(3000);
+app.use(bodyParser.urlencoded({extended: false})); 
+
+app.use('/add-products',(req, res, next) => {
+   // console.log("ONE Middleware is working");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+   // next(); //Allow the request to continue to the next middleware.
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+}); 
+
+app.use('/',(req, res, next) => {
+    console.log("THIS ALWAYS RUNS!!! EVEN WITHOUT NEXT");
+    res.send("<h1>Assalamu Alaikum, Khadizatul Kobra, My sweetest Ahlia!!!</h1>");
+});
+
+app.listen(3000);
+ //const routes = require('./routes');
+//const server = http.createServer(routes);
+//const server = http.createServer(app);
+//server.listen(3000);
